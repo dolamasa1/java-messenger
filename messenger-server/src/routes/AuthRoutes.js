4256dfd4623836b52e0b4810a944f3348108fa6f
dolamasa1 <ahmed.adel.elmoghraby@gmail.com>
2025-11-11 08:53:59 +0200

    d

--- messenger-server/src/routes/AuthRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/AuthController");
-const router = express.Router();
-
-router.post("/login", authController.login);
-router.post("/register", authController.register);
-module.exports = router;
